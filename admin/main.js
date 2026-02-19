05d3e98a85e450af7dcc4800411288e17d0bb636
saba <taku.tokuriyama2011@gmail.com>
2026-02-19 22:56:16 +0900

    管理画面が延々にローディングするバグを修正

--- admin/main.js
+++ admin/main.js
@@ -1,4 +1,4 @@
-import { db, ADMIN_PASSCODE } from "../firebase";
+import { db, ADMIN_PASSCODE } from "../firebase.js";
 import {
   collection, doc, getDoc, getDocs, setDoc, deleteDoc,
   onSnapshot, serverTimestamp
